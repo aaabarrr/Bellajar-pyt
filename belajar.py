f0685b85d2b2d11a21c930e876acb28537c2e004
Ali Akbar <83596039+aaabarrr@users.noreply.github.com>
2021-10-04 17:40:02 +0700

    DDP Pertemuan ke 2
    Belajar Paython peretemuan ke 2
    Nama : ali akbar
    Prodi : Teknik Informatika
    Rombel : TI07

---
+++ belajar.py
@@ -0,0 +1,29 @@
+
+
+
+
+# type data
+nama = "ali akbar"
+umur = 18
+nim = "0110221172"
+prodi = "teknik informatika"
+berat_badan = 66
+
+print ('nama =',nama,'\numur =',umur,'\nnim =',nim,'\nprodi =',prodi,'\nberat badan =',berat_badan)
+
+
+
+#luas persegi
+
+sisi_1 = 20
+sisi_2 = 20
+l = sisi_1*sisi_2
+print ("luas persegi :",l)
+
+
+#luas pergi panjang
+panjang = 40
+lebar  = 60
+panjang_kali_lebar = panjang * lebar
+
+print ("luas persegi panjan :",panjang_kali_lebar)
